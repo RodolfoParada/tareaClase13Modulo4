79f61ea7dbffcd6809fb2adeaf23d341dc1f6651
Vuelamarte123 <rodolfo.parada.gonzalez@gmail.com>
2021-03-31 18:37:09 -0300

    Initial commit

---
+++ src/main/kotlin/Principal.kt
@@ -0,0 +1,20 @@
+fun main(parametro: Array<String>) {
+    var conta1 = 0
+    var conta2 = 0
+    var conta3 = 0
+    var conta4 = 0
+    for(i in 1..10) {
+        print("Ingrese la cantidad de hijos de la familia:")
+        val cantidad = readLine()!!.toInt()
+        when (cantidad) {
+            0 -> conta1++
+            1 -> conta2++
+            2 -> conta3++
+            else -> conta4++
+        }
+    }
+    println("Cantidad de familias con 0 hijos: $conta1")
+    println("Cantidad de familias con 1 hijos: $conta2")
+    println("Cantidad de familias con 2 hijos: $conta3")
+    println("Cantidad de familias con más de 2 hijos: $conta4")
+}
